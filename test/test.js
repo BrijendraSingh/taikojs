b2c5bb02b124785b071048e42b7d65014a90be80
Brijendra Singh <brijendrasingh@in-pune-brijendrasingh.local>
2020-08-17 00:32:23 +0530

    mocha branch added

--- test/test.js
+++ test/test.js
@@ -2,16 +2,35 @@ const fk = require('faker')
 const TestData = require("../builder/testdata")
 const expect = require("chai").expect;
 
-const testdata = new TestData()
-.address()
-.username()
-.password()
-.firstname()
-.email()
-.build();
+var testdata;
 
 describe('Feature One', () => {
+    before(()=>{
+        console.log('inside before hook');
+        testdata = new TestData()
+        .address()
+        .username()
+        .password()
+        .firstname()
+        .email()
+        .build();
+        console.log(testdata);
+    })
+    after(()=>{
+        console.log('inside after hook');
+    })
+    beforeEach(()=>{
+        console.log('inside before each hook');
+    })
+    afterEach(()=>{
+        console.log('inside after each hook');
+    })
+
     it('Test One', async () => {
         expect(testdata).to.be.a('object');
+    });
+
+    it('Test Two', async () => {
+        expect(testdata.username).to.be.a('string');
     });
 });
