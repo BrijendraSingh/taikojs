c8b71231278c2fb8292d3aeaf7fcbe477fcc6806
Brijendra Singh <brijendrasingh@in-pune-brijendrasingh.local>
2020-08-17 18:50:42 +0530

    taiko code added in test.js

--- test/test.js
+++ test/test.js
@@ -1,9 +1,10 @@
 const expect = require("chai").expect;
-const { openBrowser, closeBrowser} = require("taiko")
+const { openBrowser, closeBrowser, write, into, $, goto} = require("taiko")
 
 describe('Wordpress Settings', () => {    
     beforeEach(async () =>{
         await openBrowser({headless:false});
+        await goto('http://localhost:8000/wp-login.php');
 
     })
 
@@ -12,6 +13,11 @@ describe('Wordpress Settings', () => {
     })
     
     it('Admin User is able to see settings', async () => {
+        var credential ={
+            username:"brijendra",
+            password:"password"
+        };
         
+        await write(credential.username, into($('user_login')));
     });
 });
